c9bda23b61a4fb990eb72d297e6facc4fe3669f2
Raghuveer Guthikonda <raghuveer.g@teleparadigm.com>
2014-04-18 12:00:53 +0530

    Removed newline at the end

--- date_fr.js
+++ date_fr.js
@@ -3,4 +3,4 @@
 Date.dayNames = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
 Date.abbrDayNames = ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.'];
 Date.monthNames = ['janvier', 'février ', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
-Date.abbrMonthNames = ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'];	
+Date.abbrMonthNames = ['janv.', 'févr.', 'mars', 'avr.', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'];
